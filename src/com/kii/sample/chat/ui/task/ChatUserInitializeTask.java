7d2dd6b19be91f7b88353ed9a82da38b79fb1617
Ryuji OCHI <ryuji.ochi@kii.com>
2013-10-30 15:04:40 +0900

    チャットユーザーの初期化処理をログイン後共通処理として統一し、ロールバックできるよう修正

--- src/com/kii/sample/chat/ui/task/ChatUserInitializeTask.java
+++ src/com/kii/sample/chat/ui/task/ChatUserInitializeTask.java
@@ -9,15 +9,19 @@ import com.kii.cloud.storage.KiiAnyAuthenticatedUser;
 import com.kii.cloud.storage.KiiPushSubscription;
 import com.kii.cloud.storage.KiiTopic;
 import com.kii.cloud.storage.KiiUser;
+import com.kii.cloud.storage.exception.ACLOperationException;
+import com.kii.cloud.storage.exception.app.ConflictException;
 import com.kii.sample.chat.ApplicationConst;
 import com.kii.sample.chat.model.ChatUser;
 import com.kii.sample.chat.util.GCMUtils;
 import com.kii.sample.chat.util.Logger;
 
 /**
- * バックグラウンドでチャットユーザーの初期化処理を実行します。
+ * バックグラウンドでユーザーサインアップ・ログイン後の初期化処理を実行します。
  */
 public class ChatUserInitializeTask extends AsyncTask<Void, Void, Boolean> {
+	
+	private static final String INITIALIZED_USER_KEY = "initialized";
 	
 	public interface OnInitializeListener {
 		public void onInitializeCompleted();
@@ -33,36 +37,79 @@ public class ChatUserInitializeTask extends AsyncTask<Void, Void, Boolean> {
 	
 	@Override
 	protected Boolean doInBackground(Void... params) {
-		try {
-			// ユーザーサインアップ後の処理は以下の5つの処理からなるが、途中で失敗した場合、リトライやロールバックの処理が必要
-			// 1.AppScope Bucketへのチャットユーザー登録
-			// 2.Pushのinstall
-			// 3.User Topicの作成
-			// 4.User TopicへのACLの設定
-			// 5.Topicの購読
-			KiiUser kiiUser = KiiUser.getCurrentUser();
-			ChatUser user = new ChatUser(kiiUser.toUri().toString(), username, email);
-			user.getKiiObject().save();
-			// GCMの設定
-			String registrationId = GCMUtils.register();
-			KiiUser.pushInstallation().install(registrationId);
-			// サーバからプッシュ通知を受信する為に、自分専用のトピックを作成する
-			// このトピックは他の全てのユーザに書き込み権限を与え
-			// 他のユーザが自分をチャットメンバー追加したことを通知する為に使用する
-			KiiTopic topic = KiiUser.topic(ApplicationConst.TOPIC_INVITE_NOTIFICATION);
-			topic.save();
-			KiiACL acl = topic.acl();
-			acl.putACLEntry(new KiiACLEntry(KiiAnyAuthenticatedUser.create(), TopicAction.SEND_MESSAGE_TO_TOPIC, true));
-			acl.save();
-			KiiPushSubscription subscription = kiiUser.pushSubscription();
-			subscription.subscribe(topic);
-			// 初期化が完了していることを示すフラグをユーザーのカスタムフィールドに追加
-			kiiUser.set("initialized", true);
-			kiiUser.update();
-			return true;
-		} catch (Exception e) {
-			Logger.e("Failed to initialize", e);
-			return false;
+		if (KiiUser.getCurrentUser().getBoolean(INITIALIZED_USER_KEY, false)) {
+			// 初期化処理が終了している場合、GCMのRegistrationIDを更新する
+			try {
+				installGCMRegistrationID();
+				return true;
+			} catch (Exception ex) {
+				Logger.e("Unable to setup the GCM.", ex);
+				return false;
+			}
+		} else {
+			// 初期化処理を行う
+			try {
+				initializeChatUser();
+				return true;
+			} catch (Exception e) {
+				Logger.e("Failed to initialize", e);
+				return false;
+			}
 		}
+	}
+	
+	private void installGCMRegistrationID() throws Exception {
+		String registrationId = GCMUtils.register();
+		KiiUser.pushInstallation().install(registrationId);
+	}
+	
+	private void initializeChatUser() throws Exception {
+		// ユーザーサインアップ後の処理は以下の5つの処理で構成される
+		// 途中で失敗したかどうかを判断するためにユーザーのプロパティに "initialized" を追加している。
+		//
+		// 1.AppScope Bucketへのチャットユーザー登録
+		// 2.Pushのinstall
+		// 3.User Topicの作成
+		// 4.User TopicへのACLの設定
+		// 5.Topicの購読
+		KiiUser kiiUser = KiiUser.getCurrentUser();
+		ChatUser user = ChatUser.findByUri(kiiUser.toUri());
+		if (user == null) {
+			user = new ChatUser(kiiUser.toUri().toString(), username, email);
+			user.getKiiObject().save();
+		}
+		// GCMの設定
+		installGCMRegistrationID();
+		// サーバからプッシュ通知を受信する為に、自分専用のトピックを作成する
+		// このトピックは他の全てのユーザに書き込み権限を与え
+		// 他のユーザが自分をチャットメンバー追加したことを通知する為に使用する
+		KiiTopic topic = KiiUser.topic(ApplicationConst.TOPIC_INVITE_NOTIFICATION);
+		try {
+			topic.save();
+		} catch (ConflictException e) {
+			// このExceptionをキャッチした場合は、Topicはすでに作成されている
+		}
+		KiiACL acl = topic.acl();
+		acl.putACLEntry(new KiiACLEntry(KiiAnyAuthenticatedUser.create(), TopicAction.SEND_MESSAGE_TO_TOPIC, true));
+		try {
+			acl.save();
+		} catch (ACLOperationException e) {
+			Throwable t = e.getCause();
+			if (t instanceof ConflictException){
+				// このExceptionをキャッチした場合は、ACLはすでに作成されている
+			} else {
+				// それ以外の場合のExceptionの場合は再度スローする
+				throw e;
+			}
+		}
+		KiiPushSubscription subscription = kiiUser.pushSubscription();
+		try {
+			subscription.subscribe(topic);
+		} catch (ConflictException e) {
+			// このExceptionをキャッチした場合は、Topicはすでに購読されている
+		}
+		// 初期化が完了していることを示すフラグをユーザーのカスタムフィールドに追加し、更新する
+		kiiUser.set(INITIALIZED_USER_KEY, true);
+		kiiUser.update();
 	}
 }
