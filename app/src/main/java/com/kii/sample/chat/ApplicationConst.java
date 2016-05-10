78fb2f7c07d88da5ee042d9d6e034f14b00684e6
nfukuzaki <noriyoshi.fukuzaki@kii.com>
2016-05-10 13:20:07 +0900

    EclipseプロジェクトをAndroidStudioプロジェクトに変換

---
+++ app/src/main/java/com/kii/sample/chat/ApplicationConst.java
@@ -0,0 +1,64 @@
+package com.kii.sample.chat;
+
+/**
+ * KiiChatアプリケーション共通で使用する定数を管理します。
+ * 
+ * @author noriyoshi.fukuzaki@kii.com
+ */
+public final class ApplicationConst {
+	
+	// TODO:APP_IDとAPP_KEYはKiiCloudでアプリケーションを作成した際に発行された値に書き換えてください。
+	public static final String APP_ID = "f39c2d34";
+	public static final String APP_KEY = "2e98ef0bb78a58da92f9ac0709dc99ed";
+	// TODO:SENDER_IDはGCMの設定を有効にした際に発行された値に書き換えてください。
+	public static final String SENDER_ID = "1012419078893";
+	// TODO:FACEBOOK_APP_IDはFacebookDevelopersでアプリケーションを作成した際に発行された値に書き換えてください。
+	// http://docs.kii.com/ja/guides/android/managing-users/social-network-integration/facebook/
+	public static final String FACEBOOK_APP_ID = "699038683441923";
+	// TODO:AGGREGATION_RULE_IDは開発者ポータルでAggregation Ruleを作成した際に発行された値に書き換えてください。
+	public static final String AGGREGATION_RULE_ID = "87";
+    // TODO: STAMP_BUTTON_ABTEST_IDは開発者ポータルでA/Bテストを作成した際に発行された値に書き換えてください。
+	public static final String ABTEST_ID = "b5388890-098c-11e4-8ad5-12314307e197";
+	
+	/**
+	 * 全てのチャットユーザが自分用に保持しているTOPICの名前です。
+	 * このトピックはユーザが個別に持つメールボックスに似ています。
+	 * 他のユーザがこのトピックにメッセージを送信すると、ユーザにプッシュ通知が送信されます。
+	 * 具体的には、チャットを開始した時に、チャットが開始されたことを相手に伝える為に使用します。
+	 * この通知を受けた相手は、チャット用に作成されたグループスコープのchat_roomバケットを購読して監視するようにします。
+	 * 
+	 * @see http://documentation.kii.com/ja/guides/android/managing-push-notification/push-to-user/
+	 */
+	public static final String TOPIC_INVITE_NOTIFICATION = "invite_notify";
+	/**
+	 * チャットが開始されたことを表すアクションです。
+	 */
+	public static final String ACTION_CHAT_STARTED = "com.kii.sample.chat.ACTION_CHAT_STARTED";
+	/**
+	 * メッセージを受信したことを表すアクションです。
+	 */
+	public static final String ACTION_MESSAGE_RECEIVED = "com.kii.sample.chat.ACTION_MESSAGE_RECEIVED";
+	/**
+	 * BroadcastReceiverが受信したPush通知をActivityに送る時に使用するキーです。
+	 */
+	public static final String EXTRA_MESSAGE = "com.kii.sample.chat.EXTRA_MESSAGE";
+
+	/**
+	 * A/Bテストのイベント名です。イベントインスタンスを作成する際に使用します。
+	 */
+    public static final String ABTEST_STAMP_BUTTON_VIEWED_EVENT = "stamp_button_viewedEvent";
+	public static final String ABTEST_STAMP_POSTED_EVENT = "stamp_postedEvent";
+
+	/**
+	 * A/Bテストの変数名です。この変数はスタンプ一覧ボタンの色を値として持ちます。
+	 */
+	public static final String ABTEST_STAMP_BUTTON_COLOR = "stamp_button_color";
+
+	/**
+	 * この値がA/Bテストの変数にセットされていた場合、A/Bテスト実行前の実装をデフォルトとして適用します。
+	 */
+	public static final String ABTEST_DEFAULT = "default";
+
+	private ApplicationConst() {
+	}
+}
---
+++ app/src/main/java/com/kii/sample/chat/ui/SigninActivity.java
@@ -0,0 +1,150 @@
+package com.kii.sample.chat.ui;
+
+import com.kii.cloud.abtesting.KiiExperiment;
+import com.kii.cloud.storage.KiiUser;
+import com.kii.cloud.storage.social.connector.KiiSocialNetworkConnector;
+import com.kii.sample.chat.R;
+import com.kii.sample.chat.model.ChatRoom;
+import com.kii.sample.chat.ui.task.ChatUserInitializeTask.OnInitializeListener;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.TextView;
+
+import com.kii.cloud.storage.Kii;
+import com.kii.cloud.storage.callback.KiiSocialCallBack;
+import com.kii.cloud.storage.social.KiiFacebookConnect;
+import com.kii.cloud.storage.social.KiiSocialConnect.SocialNetwork;
+import com.kii.sample.chat.ApplicationConst;
+import com.kii.sample.chat.PreferencesManager;
+import com.kii.sample.chat.ui.task.ABTestInfoFetchTask;
+import com.kii.sample.chat.ui.task.ChatUserInitializeTask;
+import com.kii.sample.chat.ui.util.SimpleProgressDialogFragment;
+import com.kii.sample.chat.ui.util.ToastUtils;
+import com.kii.sample.chat.util.Logger;
+
+/**
+ * サインイン画面のアクティビティです。
+ * 
+ * @author noriyoshi.fukuzaki@kii.com
+ */
+public class SigninActivity extends ActionBarActivity implements OnInitializeListener{
+	
+	private TextView textNewAccount;
+	private Button btnFbSignin;
+	private Button btnSignin;
+	private CheckBox checkRemember;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_signin);
+		
+		this.textNewAccount = (TextView)findViewById(R.id.text_new_account);
+		this.checkRemember = (CheckBox)findViewById(R.id.check_remember);
+		this.btnFbSignin = (Button)findViewById(R.id.button_facebook);
+		this.btnSignin = (Button)findViewById(R.id.button_signin);
+		
+		this.btnFbSignin.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				KiiSocialNetworkConnector connect = (KiiSocialNetworkConnector) Kii.socialConnect(SocialNetwork.SOCIALNETWORK_CONNECTOR);
+				connect.logIn(SigninActivity.this, KiiSocialNetworkConnector.Provider.FACEBOOK, new KiiSocialCallBack() {
+					@Override
+					public void onLoginCompleted(SocialNetwork network, KiiUser user, Exception exception) {
+						if (exception == null) {
+							if (checkRemember.isChecked()) {
+								Logger.i(user.getAccessToken());
+								PreferencesManager.setStoredAccessToken(user.getAccessToken());
+							}
+							new PostSigninTask(user.getDisplayname(), user.getEmail()).execute();
+						} else {
+							Logger.e("failed to sign up", exception);
+							ToastUtils.showShort(SigninActivity.this, "Unable to sign up");
+						}
+					}
+				});
+			}
+		});
+		this.btnSignin.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				// サインイン画面を表示する
+				SigninDialogFragment signinFragment = SigninDialogFragment.newInstance(SigninActivity.this, checkRemember.isChecked());
+				signinFragment.show(getSupportFragmentManager(), SigninDialogFragment.TAG);
+			}
+		});
+		this.textNewAccount.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				// サインアップ画面を表示する
+				SignupDialogFragment signupFragment = SignupDialogFragment.newInstance(SigninActivity.this);
+				signupFragment.show(getSupportFragmentManager(), "signup");
+			}
+		});
+	}
+	
+	private class PostSigninTask extends ChatUserInitializeTask {
+		
+		private PostSigninTask(String username, String email) {
+			super(username, email);
+		}
+		
+		@Override
+		protected void onPreExecute() {
+			SimpleProgressDialogFragment.show(getSupportFragmentManager(), "Signin", "Processing...");
+		}
+		
+		@Override
+		protected void onPostExecute(Boolean result) {
+			SimpleProgressDialogFragment.hide(getSupportFragmentManager());
+			if (result) {
+				// サインアップ処理が正常に行われた場合はメイン画面に遷移する
+			    new PreMoveToChatMainTask().execute();
+			} else {
+				ToastUtils.showShort(SigninActivity.this, "Unable to sign in");
+			}
+		}
+	}
+	
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		if (requestCode == KiiSocialNetworkConnector.REQUEST_CODE) {
+			Kii.socialConnect(SocialNetwork.SOCIALNETWORK_CONNECTOR).respondAuthOnActivityResult(requestCode, resultCode, data);
+		}
+	}
+	
+	/**
+	 * Chatのメイン画面に遷移する前にA/Bテストの情報取得を行います。
+	 * @author tatsuro.fujii@kii.com
+	 *
+	 */
+	private class PreMoveToChatMainTask extends ABTestInfoFetchTask {
+
+        @Override
+        protected void onPreExecute() {
+        }
+
+        @Override
+        protected void onPostExecute(KiiExperiment result) {
+            moveToChatMain(result);
+        }
+    }
+	
+	@Override
+	public void onInitializeCompleted() {
+	    new PreMoveToChatMainTask().execute();
+	}
+	
+    private void moveToChatMain(final KiiExperiment experiment) {
+        ChatRoom.ensureSubscribedBucket(KiiUser.getCurrentUser());
+        Intent intent = new Intent(SigninActivity.this, ChatMainActivity.class);
+        intent.putExtra(ChatActivity.INTENT_EXPERIMENT, experiment);
+        startActivity(intent);
+    }	
+}
